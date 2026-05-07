44e2ade491ad4528fb5797e236f4f369dde4cd3d
Tarun <sr.tarun98@gmail.com>
2026-05-07 18:48:00 +0530

    api

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        NEXT_PUBLIC_API_URL:"https://facechatappbackend.onrender.com/api"
+    }
+};
 module.exports = nextConfig;
